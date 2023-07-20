839bf261437730e61506a976813cde09d6f0440d
rithik <rithikkuvvarapu7@gmail.com>
2023-07-21 00:39:21 +0530

    update

--- src/home.js
+++ src/home.js
@@ -58,7 +58,7 @@ return(
   }
   else{
     return(
-      <h1>Loading...................</h1>
+      <img src='https://cdn.dribbble.com/users/1787505/screenshots/7300251/media/a351d9e0236c03a539181b95faced9e0.gif' alt='loader'/>
     )
   }
 }
